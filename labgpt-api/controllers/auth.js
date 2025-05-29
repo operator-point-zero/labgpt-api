89e41d8ea28daf302d968669ffeaf898505e9459
operator-point-zero <projectnoon987@gmail.com>
2025-05-29 00:25:26 -0700

    fix user import statement

--- labgpt-api/controllers/auth.js
+++ labgpt-api/controllers/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User');
+const User = require('../models/user');
 
 // POST /api/auth/oauth
 router.post('/oauth', async (req, res) => {
